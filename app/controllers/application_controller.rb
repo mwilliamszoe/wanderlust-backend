3b9d967eab7dd3f55833c3f84493bbda7270caf3
github username <github email address>
2018-12-06 11:28:48 -0600

    saves render immediately

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,10 @@ class ApplicationController < ActionController::API
         end
         if user && user.authenticate(params[:password])
         token = JWT.encode({ user_id: user.id }, nil, 'none')
-        # render :json => {user: user, experiences: user.experiences, jwt: token, user_likes: user.likes}, status: 200
-        render :json => {user: user, like: user.likes, experience: user.experiences, jwt: token,}, status: 200
-        #     render :json => {
-        #         :token => JWT.encode({ user_id: user.id }, nil, 'none')
-        #         # byebug
-        # }
+        render :json => {user: user, liked_experiences: user.liked_experiences, experience: user.experiences, jwt: token,}, status: 200
+        # render :json => @users.to_json(:include => { :liked_experiences => {:include =>:user}, :experiences })
+        # render json: @user.to_json(include: [{liked_experiences: {include: :user}}, :experiences])
+
         else
         render :json => {
             :message => "Invalid credentials"
@@ -21,26 +19,5 @@ class ApplicationController < ActionController::API
         end
     end
 
-    # def signup
-    #     user = User.create(email: params[:email], password: params[:password])
-    #     if user.valid?
-    #     render :json => {
-    #             :token => JWT.encode({ user_id: user.id }, nil, 'none')
-    #     }
-    #     else
-    #     render :json => {
-    #         :message => "Please try again"
-    #     }, status: 400
-    #     end
-    # end
-    #     if user.valid?
-    #         token = JWT.encode({ user_id: user.id }, nil, 'none')
-    #         render :json => {user: user, experiences: user.experiences, jwt: token, likes: user.likes}, status: 200
-    #         else
-    #         render :json => {
-    #             :message => "Please try again"
-    #         }, status: 400
-    #         end
-    # end
 
 end
